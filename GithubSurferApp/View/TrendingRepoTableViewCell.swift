d4deb9a68a6e682103c0c2b3c9b7c379ba95e88b
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-20 16:39:33 -0700

    iOS

---
+++ GithubSurferApp/View/TrendingRepoTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TrendingRepoTableViewCell.swift
+//  GithubSurferApp
+//
+//  Created by Veldanov, Anton on 6/20/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class TrendingRepoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+
+}
