77f155d8131e6fb42bd9e071c9c75f3f4de5c1b9
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-20 16:36:36 -0700

    iOS

--- GithubSurferApp/Model/RepoModel.swift
+++ GithubSurferApp/Model/RepoModel.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2020 Anton Veldanov. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RepoModel {
+  public private(set) var image: UIImage
+  public private(set) var name: String
+  public private(set) var description: String
+  public private(set) var numberOfForks: Int
+  public private(set) var lang: String
+  public private(set) var numberOfContributors: Int
+  public private(set) var repoUrl: String
+  
+  init(image: UIImage, name: String, description: String,numberOfForks: Int,lang: String, numberOfContributors: Int, repoUrl: String
+) {
+    self.image = image
+    self.name = name
+    self.description = description
+    self.numberOfForks = numberOfForks
+    self.lang = lang
+    self.numberOfContributors = numberOfContributors
+    self.repoUrl = repoUrl
+  }
+}
+
