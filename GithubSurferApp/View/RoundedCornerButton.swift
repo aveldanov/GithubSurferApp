5ee2f97391534dc9ad272874e362e45424ba66e0
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-20 00:00:06 -0700

    iOS

---
+++ GithubSurferApp/View/RoundedCornerButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedCornerButton.swift
+//  GithubSurferApp
+//
+//  Created by Veldanov, Anton on 6/19/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class RoundedCornerButton: UIButton {
+
+  override func awakeFromNib() {
+    
+    backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0)
+    layer.cornerRadius = frame.height/2
+    
+    layer.borderWidth = 3
+  }
+
+}
