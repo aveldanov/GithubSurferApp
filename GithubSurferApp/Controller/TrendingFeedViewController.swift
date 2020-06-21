9fc689d8ce2ca8b117f626dd697c6953d12cd2b1
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-20 20:39:16 -0700

    iOS

--- GithubSurferApp/Controller/TrendingFeedViewController.swift
+++ GithubSurferApp/Controller/TrendingFeedViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TrendingFeedViewController: UIViewController {
+class TrendingFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
   @IBOutlet weak var tableView: UITableView!
   
@@ -17,6 +17,21 @@ class TrendingFeedViewController: UIViewController {
     super.viewDidLoad()
 
   }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "trendingRepoTableViewCell") as? TrendingRepoTableViewCell else {return UITableViewCell()}
+    let repo = RepoModel(image: <#T##UIImage#>, name: "Swift", description: "Descrip", numberOfForks: 22, lang: "C++", numberOfContributors: 687, repoUrl: "www.intuit.com")
+    
+  }
+  
 
 
 }
--- GithubSurferApp/View/TrendingRepoTableViewCell.swift
+++ GithubSurferApp/View/TrendingRepoTableViewCell.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class TrendingRepoTableViewCell: UITableViewCell {
 
+  private var repoUrl: String?
+  
+  
+  @IBOutlet weak var backViewOutlet: UIView!
+  @IBOutlet weak var repoNameLabel: UILabel!
+  @IBOutlet weak var repoDescLabel: UILabel!
+  @IBOutlet weak var repoImageView: UIImageView!
+  @IBOutlet weak var numberOfForksLabel: UILabel!
+  @IBOutlet weak var langLabel: UILabel!
+  @IBOutlet weak var contribLabel: UILabel!
+  @IBOutlet weak var viewReadMeButtonOutlet: RoundedCornerButton!
+
+  
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+  
+  override func layoutSubviews() {
+    backViewOutlet.layer.cornerRadius = 15
+    backViewOutlet.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    backViewOutlet.layer.shadowOpacity = 0.25
+    backViewOutlet.layer.shadowRadius = 5.0
+    backViewOutlet.layer.shadowOffset = CGSize(width: 0, height: 0)
+  }
+  
+  
+  func configureCell(repo:RepoModel){
+    repoImageView.image = repo.image
+    repoNameLabel.text = repo.name
+    repoDescLabel.text = repo.description
+    numberOfForksLabel.text = String(describing: repo.numberOfForks)
+    langLabel.text = repo.lang
+    contribLabel.text = String(describing: repo.numberOfContributors)
+    repoUrl = repo.repoUrl
+  }
 
 
 
