61e8500a13be4ef89ba24f5b0cce9e1bc921c88b
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-20 21:53:45 -0700

    iOS

--- GithubSurferApp/Services/DownloadService.swift
+++ GithubSurferApp/Services/DownloadService.swift
@@ -7,12 +7,17 @@
 //
 
 import Foundation
+import Alamofire
 
 class DownloadService {
   static let instance = DownloadService()
   
   func downloadTrendingRepoDictArray(completion: @escaping (_ reposDictArray:[Dictionary<String,Any>])->() ){
-    
+    AF.request(trendingRepoUrl).responseJSON { (response) in
+      guard let json = response.result as? Dictionary<String,Any> else {return}
+      
+      
+    }
     
     
   }
