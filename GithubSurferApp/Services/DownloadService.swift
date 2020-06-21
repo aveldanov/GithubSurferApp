7999d1f41271d0a3f83b8c5a5f6d674d7a56331a
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-20 21:51:02 -0700

    iOS

---
+++ GithubSurferApp/Services/DownloadService.swift
@@ -0,0 +1,19 @@
+//
+//  DownloadService.swift
+//  GithubSurferApp
+//
+//  Created by Veldanov, Anton on 6/20/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import Foundation
+
+class DownloadService {
+  static let instance = DownloadService()
+  
+  func downloadTrendingRepoDictArray(completion: @escaping (_ reposDictArray:[Dictionary<String,Any>])->() ){
+    
+    
+    
+  }
+}
