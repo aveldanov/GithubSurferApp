c2612acdf5cdc25505a9976126623660fdb97a05
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-25 22:01:36 -0700

    iOS

---
+++ GithubSurferApp/View/SearchTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  SearchTableViewCell.swift
+//  GithubSurferApp
+//
+//  Created by Veldanov, Anton on 6/25/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
